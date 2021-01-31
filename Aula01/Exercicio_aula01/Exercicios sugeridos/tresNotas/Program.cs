27d363cd2ddce3cbedc7c58a3467f614776429b2
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-01-30 23:55:31 -0300

    praticando

--- Aula01/Exercicio_aula01/Exercicios sugeridos/tresNotas/Program.cs
+++ Aula01/Exercicio_aula01/Exercicios sugeridos/tresNotas/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace tresNotas
 {
     class Program
     {
-        static void Main(string[] args)
+
+        static void Main()
         {
+            //Nota1
+            Console.WriteLine("nota1");
+            double  nota1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("peso1");
+            double peso1 = double.Parse(Console.ReadLine());
+
+            //Nota2
+            Console.WriteLine("nota2");
+            double nota2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("peso2");
+            double peso2 = double.Parse(Console.ReadLine());
+            //3
+            Console.WriteLine("nota3");
+            double nota3 = double.Parse(Console.ReadLine());
+            Console.WriteLine("peso3");
+            double peso3 = double.Parse(Console.ReadLine());
+
+
+            double media = (nota1 * peso1 + nota2 * peso2 + nota3 * peso3);
+                
+            Console.WriteLine("A media do aluno e " + media );
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
+
