11bc1228530006aa593819f2e8fa972fc5af1a85
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-02 22:00:47 -0300

    adcionanod praticas

---
+++ Exercicio_aula01/Exercicios sugeridos/fatorialDez/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fatorialDez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor = 0;
+            int resposta = 1;
+            for (int i = 2; i <= valor; i++)
+            {
+                resposta *= i;
+            }
+
+            Console.WriteLine("O fatorial de " + valor + " é igual a " + resposta);
+
+            static long calcularFatorial(int num)
+            {
+                if (num == 0)
+                {
+                    // Para num == 0, o fatorial é 1.
+                    return 1;
+                }
+
+                // Caso contrário, o fatorial é o número multiplicado pelo fatorial do seu antecessor.
+                return num * calcularFatorial(num - 1);
+            }
+        }
+    }
+}
---
+++ Exercicio_aula01/Exercicios sugeridos/valordeXeY/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace valordeXeY
+{ class Program
+    {
+        static void Main ()
+
+        {
+            Console.WriteLine();
+            int x = int.Parse(Console.ReadLine());
+
+            int valor = x;
+
+            while (x < 1000)
+            {
+                if (x % 2 == 0) ;
+            }
+
+
+        }
+    }
+
+}
+
+
+
+
+
+
