fd3f1afac45f2e4b99086171824315faf41647c9
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-02 22:48:16 -0300

    ContaBancaria

--- Aula01/Exercicio_aula01/Praticas/ClassesObjetos/ContaBancaria.cs
+++ Aula01/Exercicio_aula01/Praticas/ClassesObjetos/ContaBancaria.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassesObjetos
 {
-    class Class1
+    class ContaBancaria
     {
+        public string titular;
+        public int numConta;
+        public double saldo;
+        public void Sacar(double valor)
+        {
+
+            saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            saldo -= valor;
+
+        }
+
+        public void Tranferir(double valor,ContaBancaria contaDestino)
+        {
+            Sacar(valor);
+            contaDestino.Depositar(valor);
+        }
     }
 }
