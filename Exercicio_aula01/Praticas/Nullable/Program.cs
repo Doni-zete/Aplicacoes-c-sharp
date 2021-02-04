bd22fab64e94927a1fb712c782980fa22d923289
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-03 22:31:37 -0300

    nullable

---
+++ Exercicio_aula01/Praticas/Nullable/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nullable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int? n = null;
+
+            if (n==null)
+            {
+                Console.WriteLine("Valor nulo");
+            }
+
+            if(n.HasValue)
+            {
+                Console.WriteLine("Valor nulo");
+            }
+        }
+    }
+}
