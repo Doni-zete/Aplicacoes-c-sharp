3a5125925adce5599949af16e985df83a92dd7cc
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-03 21:21:48 -0300

    parametros

--- Aula01/Exercicio_aula01/Praticas/Parametros1/Myclass.cs
+++ Aula01/Exercicio_aula01/Praticas/Parametros1/Myclass.cs
@@ -1,9 +1,16 @@
-﻿namespace Parametros1
+﻿using System;
+
+namespace Parametros1
 {
     internal class Myclass
     {
         public Myclass()
         {
+        }
+
+        internal void Trocar(int i)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- Aula01/Exercicio_aula01/Praticas/Parametros1/Program.cs
+++ Aula01/Exercicio_aula01/Praticas/Parametros1/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Parametros1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Myclass m = new Myclass();
+            int i = 5;
+            m.Trocar(ref i);
+
+            Console.WriteLine(i);
+            Numero num =  new Numero();
+            num.n =5;
+            m.mudar (num);
+
+            Console.WriteLine(num.n);
         }
     }
+
+    class MyClass
+    {
+        public void Trocar(ref int x)
+        {
+            x = 10;
+        }
+        public void Mudar (ref Numero x)
+            {
+           // x.n =10;
+           x =new Numero();
+            x.n =20;
+        }
+    }
+
+    class Numero
+	{
+        public int n;
+	}
 }
