7bb254ed940b38d3e9cc19db8ef7a5d8637878a5
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-09 23:38:21 -0300

    AJUSTE

---
+++ Exercicio_aula01/Exercicios sugeridos/Relogio e Ponteiro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Relogio_e_Ponteiro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
