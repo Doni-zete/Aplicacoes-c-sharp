dfb91d265fe2f1b71e6fe53307e631e7bfcfabfc
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-14 22:26:29 -0300

    cosntrutores

---
+++ Exercicio_aula01/Praticas/Construtores/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Construtores
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Salario s = new Salario();
+
+            Console.WriteLine(s.valor);
+            Console.WriteLine(s.mes);
+        }
+    }
+
+    class Salario
+    {
+        public double valor;
+        public int mes;
+        public Salario(double valor, double bonus)
+        {
+            Console.WriteLine("Criando objeto");
+
+            this.valor = valor + valor * bonus;
+
+        }
+        public void MostrarValor()
+        {
+            Console.WriteLine(valor);
+        }
+    }
+
+
+
+}
