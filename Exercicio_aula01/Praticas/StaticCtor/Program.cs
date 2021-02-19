b99c1f5d28200724276f364a55f147ae31aa1531
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-18 23:01:19 -0300

    static

--- Exercicio_aula01/Praticas/StaticCtor/Program.cs
+++ Exercicio_aula01/Praticas/StaticCtor/Program.cs
@@ -9,21 +9,15 @@ namespace StaticCtor
         {
             Console.WriteLine(Cambio.cotacaoDolar);
             Console.WriteLine(Cambio.cotacaoDolar);
-
         }
     }
-
     class Cambio
     {
         public static double cotacaoDolar;
         static Cambio()
         {
-
-            Console.WriteLine(Cambio.cotacaoDolar);
+            Console.WriteLine("Construtor chamado");
             cotacaoDolar = 2.1;
-          
-
         }
-
     }
 }
