90d421c3dd3398db19be4caccfe9bbe399e301c6
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-18 23:05:31 -0300

    constants

---
+++ Exercicio_aula01/Praticas/Constants/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace Constants
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(Matematica.PI);
+
+        }
+    }
+
+    class Matematica
+    {
+        public const double PI = 3.1416;
+    }
+}
