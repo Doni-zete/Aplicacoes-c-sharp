54ccb0fa69479672619c82e6fadfed070b9c1f9d
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-02-26 23:26:48 -0300

    proprierties

--- Exercicio_aula01/Praticas/Proprierties.Endereco/Proprierties.Endereco/Program.cs
+++ Exercicio_aula01/Praticas/Proprierties.Endereco/Proprierties.Endereco/Program.cs
@@ -1,15 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Proprierties.Endereco
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            Endereco e = new Endereco();
+            e.Logradouro = "R. dos Programadores";
+            e.numero = 250;
+            e.apartamento = true;
+
         }
+    }
+class Endereco
+    {
+        private string logradouro;
+        private int numero;
+        private bool apartamento;
+
+        public string Logradouro
+        {
+            get
+            {
+                return logradouro;
+            }
+            set
+            {
+                logradouro = value;
+
+            }
+        }
+
     }
 }
