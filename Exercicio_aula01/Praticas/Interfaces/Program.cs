7b19e7a2da7fd7f785f4515d75b3a51660d5cef1
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-03-22 23:56:14 -0300

    interface

---
+++ Exercicio_aula01/Praticas/Interfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+
+    public interface Motorizado
+    {
+        void Ligar();
+        string NomeMotor { get; set; }
+    }
+}
