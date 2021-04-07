3d33d8ea50d4ab49851ba3d8c9c079d318d1d9e6
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-04-06 22:23:35 -0300

    polimorfismo

---
+++ Exercicio_aula01/Praticas/Polimorfismo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Cachorro c = new Cachorro();
+            c.Falar();
+
+            Gato g = new Gato();
+            g.Falar();
+
+        }
+    }
+    public class Animal
+    {
+        public virtual void Falar()
+        {
+            Console.WriteLine("---");
+        }
+    }
+
+    public class Cachorro : Animal
+    {
+        public override void Falar()
+        {
+            Console.WriteLine("AU-AU");
+        }
+    }
+
+
+
+
+    public class Gato : Animal
+
+
+    public override void Falar()
+    {
+        Console.WriteLine("MIAU");
+    }
+}
