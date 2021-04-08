43df9d3bf8d206a0a2f1dbc128b05168cf0a62d3
Donizete Crisostomo <69015478+Doni-zete@users.noreply.github.com>
2021-04-07 23:46:03 -0300

    polimorfismoInterface

--- Exercicio_aula01/Praticas/PolimorfismoInterface/Program.cs
+++ Exercicio_aula01/Praticas/PolimorfismoInterface/Program.cs
@@ -10,6 +10,36 @@ namespace PolimorfismoInterface
     {
         static void Main(string[] args)
         {
+            Selo s = new Selo();
+            ImprimirColecao(s);
+
+            Bones b = new Bones();
+            ImprimirColecao(b);
+        }
+
+        static void ImprimirColecao(IColecionavel c)
+        {
+            Console.WriteLine("Esta é uma colecão de " + c.GetNomeColecao());
         }
     }
+    public interface IColecionavel
+    {
+        string GetNomeColecao();
+    }
+    public class Selo : IColecionavel
+    {
+        public string GetNomeColecao()
+        {
+            return "selos";
+        }
+    }
+
+    public class Bones : IColecionavel
+    {
+        public string GetNomeColecao()
+        {
+            return "Bones";
+        }
+    }
+
 }
